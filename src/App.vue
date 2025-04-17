d49bf6ac7714c9adb8a339d58526b6d599c957a9
xu xuejian <2498492824@qq.com>
2025-04-17 17:54:40 +0800

    测试不符规范的提交

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
 
-const a = 10;
-const number = 10;
-console.log(
-  "loremasdasdasdasdasdasdasddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"
-);
+// 不符合prettier的semi: true 末尾添加分号 规范
+const name = "Tom";
+console.log(name);
+
+// 变量未使用，不符合eslint, 无法自动修复，提交以后只会 提示 + 报错，但不会自动删除变量
+// const a = 10;
+// const number = 10;
+
 // npm install -D postcss postcss-html postcss-scss stylelint stylelint-config-recommended stylelint-config-recommended-scss stylelint-config-recommended-vue stylelint-config-recess-order stylelint-config-html stylelint-prettier
 </script>
 
@@ -35,7 +38,9 @@ console.log(
   filter: drop-shadow(0 0 2em #42b883aa);
 }
 
-/* 测试stylelint */
+/* 测试stylelint 
+width 放置在定位属性 position 前面不符合css书写顺序规范
+*/
 .container {
   position: relative;
   width: 100%;
